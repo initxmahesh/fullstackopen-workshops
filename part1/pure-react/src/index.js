414eec628ffdb894b9b2ba0db82412e644cf16c3
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-29 22:21:02 +0545

    pure react is demonstrated

--- part1/pure-react/src/index.js
+++ part1/pure-react/src/index.js
@@ -2,6 +2,15 @@ const storage = document.getElementById('root')
 
 const root = ReactDOM.createRoot(storage);
 
-let App = React.createElement('h1', { className: 'mahesh' }, "Say Hi to Me")
+let SayHello = (props) => {
+    return (React.createElement('h1', { className: 'mahesh' }, `Say Hi to Me: ${props.name}`)
+    )  
+}
 
-root.render(App)
+let App = () => {
+   return (
+        [React.createElement('div', {}, React.createElement(SayHello, { name: 'Mahesh' })), React.createElement('div', {}, React.createElement(SayHello, { name: 'Bijay' })), React.createElement('div', {}, React.createElement(SayHello, { name: 'Lokesh' }))]
+    )
+}
+
+root.render(React.createElement(App))
